ad0efd6f4d20e803fbb33cea5e86adbcc395b090
jackpitts <jackpitts61@gmail.com>
2023-05-17 16:05:28 +0200

    fixed weather text

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use(express.static(publicDirectoryPath))
 app.get("", (req, res) => {
     res.render("index", {
         title: "Weather",
-        msg: "Use this site to get your weather!",
+        msg: "Type an address, get the weather!",
         name: "Jacopo"
     })
 })
